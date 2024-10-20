a68c0e842539b84dae8a2798896b10d36060063c
binaryFox <binaryfox333@gmail.com>
2024-10-20 19:34:09 +0530

    Create app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server listening on ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Database connection failed: " + error.message);
+    });
